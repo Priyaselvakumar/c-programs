5d40862138282038c6e89a1030d45fb22318eb82
Priya Selvakumar <21041279+Priyaselvakumar@users.noreply.github.com>
2017-08-23 21:57:33 +0530

    Create diff of btw sum of odd&even digits.c

---
+++ diff of btw sum of odd&even digits.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+{
+    int i,s=0,e=0;
+    int t;
+    int n;
+    scanf("%d",&n);
+   
+    while(n)
+    {
+        t=n%10;
+        n=n/10;
+        if(t%2!=0)
+        {
+            s=s+t;
+        }
+        else
+        {
+            e=e+t;
+        }
+       
+    }
+    printf("%d",s-e);
+    
+}
